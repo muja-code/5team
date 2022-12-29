d9bf8f73f3721877437b6dc49f24b88e76adec42
Park Mu Hyun <pla2697@gmail.com>
2022-12-29 15:02:42 +0900

    aaaaa

---
+++ app.js
@@ -0,0 +1,17 @@
+// var age = 20;
+// let name = 'Max';
+
+// function greet() {
+//   let name = 'Manuel';
+//   var age = 13;
+//   console.log(name, age);
+// }
+
+// greet();
+// console.log(age);
+
+a1 = 'a1';
+console.log(a2);
+var a2 = 'a2';
+console.log(a3);
+let a3 = 'a3';
